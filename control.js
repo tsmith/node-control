21f7f1155783570de0dafc18978c3ead77ec17bd
Tom Smith <tsmith@workbench.thomassmith.com>
2010-07-16 12:09:02 +0000

    Initial commit.

---
+++ control.js
@@ -0,0 +1,10 @@
+/*global require, exports */
+
+var task = require('./lib/task'),
+    host = require('./lib/host');
+
+exports.task = task.task;
+exports.begin = task.begin; 
+exports.perform = task.perform;
+exports.hosts = host.hosts;
+exports.host = host.perform;
